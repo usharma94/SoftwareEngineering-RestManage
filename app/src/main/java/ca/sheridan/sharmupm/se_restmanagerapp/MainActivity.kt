98343fb30f8ea0aada21166e77fa464d3130e8ab
Upma <Upma.Sharma@ontario.ca>
2020-10-07 12:20:28 -0400

    Master Branch Blank Android Studio Project. Using Pixel C tablet, Oreo OS

---
+++ app/src/main/java/ca/sheridan/sharmupm/se_restmanagerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.sheridan.sharmupm.se_restmanagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
